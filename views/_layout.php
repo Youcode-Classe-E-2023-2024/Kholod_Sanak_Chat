38ad8206696905b1bfbc7c7a8e9bf1f278116374
kholoud001 <kholoud.sanak@gmail.com>
2023-12-25 11:06:31 +0100

    layout

--- views/_layout.php
+++ views/_layout.php
@@ -6,10 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= ucfirst($page) ?></title>
-    <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
-</head>
-<body>
-    <h1><?= ucfirst($page) ?> View</h1>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Register /login  style link -->
+    <link rel="stylesheet" href="<?= PATH ?>assets/css/login.css">
+   <!-- <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">-->
+
+    <!-- Profil style -->
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+    <!-- Form css -->
+
+
+<body >
 
     <main>
         <?php include_once 'views/' . $page . '_view.php'; ?>
