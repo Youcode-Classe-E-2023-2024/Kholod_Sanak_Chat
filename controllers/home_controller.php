89c156ad9d996bf63cb09e1d5bba43fd897bab93
kholoud001 <kholoud.sanak@gmail.com>
2023-12-26 16:10:39 +0100

    home controller update

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,10 +1,24 @@
 <?php
 
+$db = mysqli_connect('localhost', 'root', '', 'chat');
 
-if (isset($_POST['login'])) {
-    echo 'Is login';
+if (file_exists('../_classes/Friend_request.php')) {
+    include_once '../_classes/Friend_request.php';
+    session_start();
 }
 
-if (isset($_POST['logout'])) {
-    echo 'Is logout';
+if (isset($_POST['user_id'])) {
+    $senderId = $_SESSION['user_id'];
+    $receiverId = $_POST['user_id'];
+
+
+//     Attempt to send a friend request
+    $result = Friend_request::sendRequest($senderId, $receiverId);
+
+//    if ($result) {
+//        echo 'Friend request sent successfully';
+//    } else {
+//        echo 'Failed to send friend request';
+//    }
 }
+
