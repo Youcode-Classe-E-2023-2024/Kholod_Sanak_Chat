36d1e9d57f2bf97f15ba720f66b212c56cf7beb7
kholoud001 <kholoud.sanak@gmail.com>
2023-12-26 11:01:50 +0100

    add room ajax

--- views/home_view.php
+++ views/home_view.php
@@ -116,13 +116,16 @@
                 <img src="https://cdn.discordapp.com/embed/avatars/4.png" alt="">
             </div>
         </div>
-        <div id="addRoomTrigger" class="cursor-pointer" >
+
+
+        <div id="addRoomTrigger" class="cursor-pointer mb-4" >
             <div class="bg-white opacity-25 h-12 w-12 flex items-center justify-center text-black text-2xl font-semibold rounded-3xl mb-1 overflow-hidden">
                 <svg class="fill-current h-10 w-10 block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z" />
                 </svg>
             </div>
         </div>
+
 
     </div>
     <!-- Sidebar End-->
@@ -142,7 +145,7 @@
     $(document).ready(function () {
         $("#addRoomTrigger").on("click", function () {
             $.ajax({
-                url: 'views/add_room_view.php',
+                url: 'views/roomadd_view.php',
                 type: 'GET',
                 success: function (data) {
                     $('.flex-1 .overflow-y-scroll').html(data);
